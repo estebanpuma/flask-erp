c12e215969375abbe6fd888f80f385c99dcf4218
Esteban Puma <estepuma94@gmail.com>
2024-11-01 03:29:57 -0500

    actualizacion modelos production

--- app/production/models.py
+++ app/production/models.py
@@ -11,8 +11,6 @@ class ProductionOrder(BaseModel, SoftDeleteMixin):
     status = db.Column(db.String(50), default='pending') 
     production_batches = db.relationship('ProductionBatch', back_populates='production_order', cascade='all, delete-orphan')
     status = db.Column(db.String(50), default='pending')
-
-    bom = db.relationship('BillOfMaterials', back_populates='production_order', uselist=False)
     
 
 class ProductionBatch(BaseModel, SoftDeleteMixin):
@@ -39,7 +37,6 @@ class SaleProductionBatch(ProductionBatch):
     __tablename__ = 'sale_production_batch'
     id = db.Column(db.Integer, db.ForeignKey('production_batches.id'), primary_key=True)
     sale_order_code = db.Column(db.String(50), db.ForeignKey('sale_orders.order_number'), nullable=False)
-    products_list = db.relationship('SaleOrderProductList')
 
     __mapper_args__ = {'polymorphic_identity': 'sales_batch'}
 
@@ -48,67 +45,43 @@ class StockProductionBatch(ProductionBatch):
 
     __tablename__ = 'stock_product_batch'
     id = db.Column(db.Integer, db.ForeignKey('production_batches.id'), primary_key=True)
-    stock_order_code = db.Column(db.String(50), db.ForeignKey('stock_orders.stock_order_code'), nullable=False)
+    stock_order_id = db.Column(db.Integer, db.ForeignKey('stock_orders.id'), nullable=False)
     # Relación bidireccional entre StockProductionBatch y StockOrderProductList
-    products_list = db.relationship('StockOrderProductList', back_populates='stock_production_batch', cascade='all, delete-orphan')
+    stock_order = db.relationship('StockOrder', back_populates='stock_production_batch')
     #parámetro cascade='all, delete-orphan' para que, si un StockProductionBatch es eliminado, sus elementos relacionados en StockOrderProductList también lo sean.
 
     __mapper_args__ = {'polymorphic_identity': 'stock_batch'}
 
 
+class StockOrder(BaseModel):
+
+    __tablename__ = 'stock_orders'
+    id = db.Column(db.Integer, primary_key=True)
+    stock_order_code = db.Column(db.String(50), nullable=False, unique=True)
+    request_date = db.Column(db.Date, nullable=False)
+    responsible_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    notes = db.Column(db.String(), nullable=True)
+    
+    stock_order_product_list = db.relationship('StockOrderProductList', back_populates='stock_order')
+    stock_production_batch = db.relationship('StockProductionBatch', back_populates='stock_order')
+    
+
 class StockOrderProductList(BaseModel):
     
     __tablename__ = 'stock_order_product_list'
 
-    stock_order = db.Column(db.String(50), db.ForeignKey('stock_orders.stock_order_code'), nullable=False)
-    stock_production_batch_id = db.Column(db.Integer, db.ForeignKey('stock_product_batch.id'))
-    stock_production_batch = db.relationship('StockProductionBatch', back_populates='products_list')  # Relación bidireccional
-    product_code = db.Column(db.String(50), db.ForeignKey('products.code'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    stock_order_id = db.Column(db.Integer, db.ForeignKey('stock_orders.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     product_size = db.Column(db.Integer, nullable=False)
     product_qty = db.Column(db.Integer, nullable=False)
     notes = db.Column(db.Text, nullable=True)
 
-
-class StockOrder(BaseModel):
-
-    __tablename__ = 'stock_orders'
-
-    stock_order_code = db.Column(db.String(50), nullable=False, unique=True)
-    stock_order_product_list = db.relationship('StockOrderProductList')
+    stock_order = db.relationship('StockOrder', back_populates='stock_order_product_list')
 
 
-class SaleOrderProductList(BaseModel):
-
-    __tablename__ = 'sale_order_product_list'
-
-    sale_production_batch_id = db.Column(db.Integer, db.ForeignKey('sale_production_batch.id'))
-    sale_production_batch = db.relationship('SaleProductionBatch', back_populates='products_list') 
-
-    sale_order_code = db.Column(db.String(50), db.ForeignKey('sale_orders.order_number'), nullable=False)
-    product_code = db.Column(db.String(50), db.ForeignKey('products.code'), nullable=False)
-    product_size = db.Column(db.Integer, nullable=False)
-    product_qty = db.Column(db.Integer, nullable=False)
-    notes = db.Column(db.Text, nullable=True)
 
 
-class BillOfMaterials(BaseModel):
-    __tablename__ = 'bill_of_materials'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(50), nullable=False, unique=True)
-    materials = db.relationship('BOMMaterial', back_populates='bom', cascade='all, delete-orphan')
-    production_order_id = db.Column(db.Integer, db.ForeignKey('production_orders.id'), nullable=True)  # Si aplica a la producción general
-    production_batch_id = db.Column(db.Integer, db.ForeignKey('production_batches.id'), nullable=True)  # Si aplica a un lote específico
-    production_order = db.relationship('ProductionOrder', back_populates='bom')
 
 
-class BOMMaterial(BaseModel):
-    __tablename__ = 'bom_materials'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    material_code = db.Column(db.String(50), db.ForeignKey('materials.code'), nullable=False, unique=True)
-    material_unit = db.Column(db.String())
-    material_qty = db.Column(db.Float, nullable=False)
-    bom_id = db.Column(db.Integer, db.ForeignKey('bill_of_materials.id'), nullable=False)
-    bom = db.relationship('BillOfMaterials', back_populates='materials')
-    material = db.relationship('Material')
+
